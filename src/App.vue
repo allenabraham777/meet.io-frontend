fc8cc43de108d720ae54c05b87893ec49d7a6121
Allen K Abraham <allenabraham777@gmail.com>
2020-02-09 07:09:34 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="app">
+    <div class="mic-container">
+      <SpeechToText @speech="onSpeechReceived($event)"/>
+    </div>
+    <div class="text-container">
+      <div class="logo-wrapper">
+        <img src="@/assets/logo.svg" class="logo">
+      </div>
+      <form style="text-align:center;" action = "/hello" method = "post" enctype="multipart/form-data">  
+        <div class="text-wrapper">
+          <textarea class="identified-text" name="speech" v-model="speech" readonly></textarea>
+        </div>
+        <div class="summerize-wrapper">
+          <button type="submit" class="summerize-btn">Summerize</button>
+        </div>
+    </form>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { SpeechToText } from 'vue-voice';
+import SpeechToText from './lib/components/SpeechToText.vue';
+
+export default {
+    name: 'app',
+    components: {
+        SpeechToText
+    },
+    data() {
+        return {
+            speech: ''
+        };
+    },
+    methods: {
+        onSpeechReceived(speech) {
+            console.log('Reveived', speech);
+            this.speech = speech;
+        }
+    }
+};
+</script>
+
+<style>
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    position: fixed;
+    width: 100%;
+  }
+  #app {
+    display: flex;
+    flex-direction: row;
+  }
+  .mic-container {
+    width: 50%;
+    height: 100vh;
+    display: flex;
+    direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+  .text-container{
+    width: 50%;
+    display: flex;
+    background: #173F5F;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .logo{
+    height: 5rem;
+    margin: 2rem;
+  }
+  .logo-wrapper{
+    width: 100%;
+    text-align: right;
+  }
+  .identified-text {
+    resize: none;
+    padding: 3rem;
+    width: 40rem;
+    height: 40rem;
+    background: none;
+    border: solid 2px #ffffff;
+    color: #ffffff;
+    font-size: 2rem;
+  }
+  .summerize-btn {
+    margin: 5rem;
+    background: none;
+    height: 5rem;
+    width: 20rem;
+    font-size: 2rem;
+    color: #ffffff;
+    border: solid 2px #ffffff;
+    border-radius: 2.5rem;
+    outline: none;
+    cursor: pointer;
+    transition: all 0.2s;
+  }
+  .summerize-btn:hover {
+    color: #173F5F;
+    background: #ffffff;
+  }
+</style>
---
+++ src/lib/components/SpeechToText.vue
@@ -0,0 +1,114 @@
+<template>
+  <div id="SpeechToText">
+    <!-- eslint-disable -->
+    <div class="speech-to-text" @click="onClick()" :class="{ 'speech-to-text__button--speaking': isSpeaking }">
+      <div class="speech-to-text-1">
+        <div class="speech-to-text-2">
+          <div>
+              <img class="mic-img" v-if="!isSpeaking" :src="micImage">
+              <img v-if="isSpeaking"
+                 class="mic-img" :src="stopImage">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SpeechToText from '../services/speech-to-text';
+import micImage from '../../assets/mic.svg';
+import stopImage from '../../assets/stop.svg';
+// const micImage = require('../../assets/mic.svg');
+// const stopImage = require('../../assets/stop.svg');
+
+/*
+// eslint-disable-next-line no-unused-vars
+
+// eslint-disable-next-line no-unused-vars
+
+*/
+
+
+export default {
+    name: 'SpeechToText',
+    data() {
+        return {
+            isSpeaking: false,
+            speech: '',
+            speechService: {},
+            micImage,
+            stopImage
+        };
+    },
+    methods: {
+        onClick() {
+          if(this.isSpeaking === false) {
+
+            this.isSpeaking = true;
+            this.speechService.speak().subscribe(
+              (result) => {
+                this.speech = result;
+                    this.$emit('speech', this.speech);
+                    this.isSpeaking = false;
+                    // console.log('Result', result);
+                },
+                (error) => {
+                  console.log('Error', error);
+                    this.isSpeaking = false;
+                },
+                () => {
+                  this.isSpeaking = false;
+                }
+            );
+            console.log('speechService started');
+          }
+          else{
+            this.speechService.stopspeak();
+          }
+        }
+    },
+    props: {
+        msg: String
+    },
+    created() {
+        this.speechService = new SpeechToText();
+    }
+};
+</script>
+<style>
+.speech-to-text,.speech-to-text-1,.speech-to-text-2 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    cursor: pointer;
+    transition: all 0.2s;
+}
+
+.mic-img {
+  width: 5rem;
+}
+
+.speech-to-text:hover,.speech-to-text-1:hover,.speech-to-text-2:hover {
+  transform: scale(1.1);
+}
+
+.speech-to-text {
+  background: #3CAEA3;
+  width: 30em;
+  height: 30em;
+}
+
+.speech-to-text-1 {
+  background: #76BBB4;
+  width: 20em;
+  height: 20em;
+}
+
+.speech-to-text-2 {
+  background: #C0F4EF;
+  width: 10em;
+  height: 10em;
+}
+</style>
